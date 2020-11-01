fe8fc725cd20ac9d7bf17709b8c8ceba5ca6f5ce
leonce-m <lem2300@thi.de>
2020-11-01 14:52:52 +0100

    Refactor VCS to VIO and add VCS child class

--- dev/voice_ctrl_sys.py
+++ dev/voice_ctrl_sys.py
@@ -14,20 +14,23 @@ def foo4(verb, nouns):
     return verb + nouns.join(" ") + ": foo4"
 
 
-class VCS(object):
+class VIO(object):
+    VIO_GRAMMAR_VERBS = [
+        ({"climb", "descend", "maintain"}, "ALT", foo1),
+        ({"turn"}, "DIR", foo2),
+        ({"hold"}, "LOC", foo3),
+        ({"direct"}, "LOC", foo4)
+    ]
+    VIO_GRAMMAR_NOUNS = {
+        "ALT": {"FL [0-9]", "[0-9]+ ft"},
+        "DIR": {"heading [0-9]"},
+        "LOC": {"Ingolstadt Main Station", "MIQ", "OTT VOR", "WLD VOR"}
+    }
+
     def __init__(self, call_sign):
         self.call_sign = call_sign
-        self.verbs = [
-            ({"climb", "descend", "maintain"}, "ALT", foo1),
-            ({"turn"}, "DIR", foo2),
-            ({"hold"}, "LOC", foo3),
-            ({"direct"}, "LOC", foo4)
-        ]
-        self.nouns = {
-            "ALT": {"FL [0-9]", "[0-9]+ ft"},
-            "DIR": {"heading [0-9]"},
-            "LOC": {"Ingolstadt Main Station", "MIQ", "OTT VOR", "WLD VOR"}
-        }
+        self.verbs = self.VIO_GRAMMAR_VERBS
+        self.nouns = self.VIO_GRAMMAR_NOUNS
 
     def find_next_verb(self, token):
         for i, t in enumerate(token):
@@ -37,15 +40,10 @@ class VCS(object):
                         return i, t
         return 0, 0
 
-    def handle_id(self, token):
-        if len(token) > 1 and token[1].isdigit():
-            token[0] = token[0] + token[1]
-            token.remove(token[1])
-        if token[0] == self.call_sign:
-            token.remove(token[0])
-            return True
+    def handle_phrase(self, phrase):
+        return 0
 
-    def handle_phrase(self, token):
+    def handle_phrase_queue(self, token):
         if len(token) == 0:
             return False
         i, verb = self.find_next_verb(token)
@@ -61,19 +59,33 @@ class VCS(object):
         print(phrase)
         return True
 
+    def handle_id(self, token):
+        if len(token) > 1 and token[1].isdigit():
+            token[0] = token[0] + token[1]
+            token.remove(token[1])
+        if token[0] == self.call_sign:
+            token.remove(token[0])
+            return True
+
     def handle_command(self, command):
         token = command.split()
         if not self.handle_id(token):
             print(f"Call sign '{token[0]}' not recognized")
         else:
             while len(token) > 0:
-                if not self.handle_phrase(token):
+                if not self.handle_phrase_queue(token):
                     print(f"Command '{token[0]}' not recognized or not executable")
                     break
 
 
-def main():
-    vcs = VCS("CityAirbus1234")
+class VCS(VIO):
+    def __init__(self, call_sign, verbose=False):
+        super().__init__(call_sign)
+        self.verbose = verbose
+
+
+def main(ARGS):
+    vcs = VCS("CityAirbus1234", ARGS.verbose)
     while True:
         command = input()
         if not command:
@@ -85,4 +97,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description="Control PIXHAWK via MavSDK-Python with ATC commands (and respond)")
+    parser.add_argument('-vv','--verbose', type=bool, default=False,
+                        help="Enable verbose console output for debug purposes")
+    ARGS = parser.parse_args()
+    main(ARGS)
