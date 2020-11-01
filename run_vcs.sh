cc6a4f77c5aab108e573c51e5ee07fea2670b348
leonce-m <lem2300@thi.de>
2020-11-01 16:07:56 +0100

    Revert "Fixed not using python3 to call modules"
    This reverts commit 1c16e8dac1d71366d1b74d68a67cab89d1d12a21.

--- run_vcs.sh
+++ run_vcs.sh
@@ -1,3 +1,5 @@
 cd ./dev
-python3 mic_vad_streaming.py -m ../models/deepspeech-0.8.1-models.pbmm -d "$1" -r 44100 -k
-python3 voice_ctrl_sys.py
+chmod +x mic_vad_streaming.py
+chmod +x voice_ctrl_sys.py
+./mic_vad_streaming.py -m ../models/deepspeech-0.8.1-models.pbmm -d "$1" -r 44100 -k
+./voice_ctrl_sys.py
