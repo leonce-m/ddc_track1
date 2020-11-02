261eb77b3c8a3fed8260b0e7094dc2e36e7fabfa
leonce-m <lem2300@thi.de>
2020-11-03 00:58:31 +0100

    Work on mavskd_test

--- dev/mavsdk_test.py
+++ dev/mavsdk_test.py
@@ -3,32 +3,50 @@ import concurrent.futures
 from mavsdk import System
 
 
-async def make_async(self, sync_function):
-    executor = concurrent.futures.ThreadPoolExecutor()
-    await loop.run_in_executor(executor, sync_function)
-
-
 class VCS:
     def __init__(self, drone):
+        self.debug = True
         self.drone = drone
 
-    def get_message(self):
-        command = input()
-        token = command.split()
+    def print(self, message):
+        if self.debug:
+            print(message)
+
+    async def startup(self):
+        self.print("Initializing...")
+        system_address = "udp://:14550"
+        await self.drone.connect(system_address=system_address)
+        self.print(f"{system_address} waiting for connection...")
+        async for state in self.drone.core.connection_state():
+            self.print("...")
+            await asyncio.sleep(1)
+            if state.is_connected:
+                self.print(f"Connected to {system_address}!")
+                break
+        self.print("Arming drone...")
+        await self.drone.action.arm()
+
+    async def main_loop(self):
+        await self.handle_state()
+        command = await make_async(input())
+        await self.handle_command(command)
+        await asyncio.sleep(1)
+
+    async def handle_state(self):
+        self.print("Handling state...")
+
+    async def handle_command(self, command):
+        self.print("Command received: " + command)
 
 
-async def run(self):
-    drone = System()
-    vcs = VCS(drone)
-    await drone.connect(system_address="udp://:14540")
-    async for state in drone.core.connection_state():
-        if state.is_connected:
-            break
-    await make_async(self.get_message())
-    await drone.action.arm()
-    await drone.action.takeoff()
+async def make_async(sync_function):
+    executor = concurrent.futures.ThreadPoolExecutor()
+    value = await asyncio.get_event_loop().run_in_executor(executor, sync_function)
+    print(value)
 
 
 if __name__ == "__main__":
-    asyncio.ensure_future(run())
+    vcs = VCS(System())
+    asyncio.get_event_loop().run_until_complete(vcs.startup())
+    asyncio.ensure_future(vcs.main_loop())
     asyncio.get_event_loop().run_forever()
