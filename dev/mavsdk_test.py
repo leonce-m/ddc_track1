f686ad5075b208927430919ba1bbdc3e2dbb4492
leonce-m <lem2300@thi.de>
2020-11-02 20:49:22 +0100

    Work on mavskd_test

--- dev/mavsdk_test.py
+++ dev/mavsdk_test.py
@@ -3,26 +3,30 @@ import concurrent.futures
 from mavsdk import System
 
 
-def get_message():
-    return input()
-
-
-async def make_async(sync_function):
+async def make_async(self, sync_function):
     executor = concurrent.futures.ThreadPoolExecutor()
     await loop.run_in_executor(executor, sync_function)
 
 
-async def run():
+class VCS:
+    def __init__(self, drone):
+        self.drone = drone
+
+    def get_message(self):
+        command = input()
+        token = command.split()
+
+
+async def run(self):
     drone = System()
+    vcs = VCS(drone)
     await drone.connect(system_address="udp://:14540")
     async for state in drone.core.connection_state():
         if state.is_connected:
             break
-    await make_async(get_message())
+    await make_async(self.get_message())
     await drone.action.arm()
     await drone.action.takeoff()
-    await asyncio.sleep(5)
-    await drone.action.land()
 
 
 if __name__ == "__main__":
