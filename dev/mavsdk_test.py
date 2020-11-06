288356561e0244772afc22e3f0ecacc73be002f5
leonce-m <lem2300@thi.de>
2020-11-06 02:32:00 +0100

    Add async stdin.readLine to mavskd_test

--- dev/mavsdk_test.py
+++ dev/mavsdk_test.py
@@ -1,42 +1,52 @@
+import sys
 import asyncio
 import concurrent.futures
 from mavsdk import System
-
+from concurrent.futures import ThreadPoolExecutor
 
 class VCS:
     def __init__(self, drone):
-        self.debug = True
+        self.verbose = True
+        self.listen = True
         self.drone = drone
 
-    def print(self, message):
-        if self.debug:
+    async def ainput(self, prompt: str = ""):
+        if not self.listen:
+            return
+        with ThreadPoolExecutor(1, "AsyncInput", lambda x: print(x, end="", flush=True), (prompt,)) as executor:
+            return (await asyncio.get_event_loop().run_in_executor(
+                executor, sys.stdin.readline
+            )).rstrip()
+
+    async def aprint(self, message):
+        if self.verbose:
             print(message)
 
-    async def startup(self):
-        self.print("Initializing...")
+    async def start(self):
+        await self.aprint("Initializing...")
         system_address = "udp://:14550"
         await self.drone.connect(system_address=system_address)
-        self.print(f"{system_address} waiting for connection...")
+        await self.aprint(f"{system_address} waiting for connection...")
         async for state in self.drone.core.connection_state():
-            self.print("...")
+            await self.aprint("...")
             await asyncio.sleep(1)
             if state.is_connected:
-                self.print(f"Connected to {system_address}!")
+                await self.aprint(f"Connected to {system_address}!")
                 break
-        self.print("Arming drone...")
+        await self.aprint("Arming drone...")
         await self.drone.action.arm()
 
     async def run(self):
         await self.handle_state()
-        command = await make_async(input)
+        command = await self.ainput()
         await self.handle_command(command)
         await asyncio.sleep(1)
 
     async def handle_state(self):
-        self.print("Handling state...")
+        await self.aprint("Handling state...")
 
     async def handle_command(self, command):
-        self.print("Command received: " + command)
+        await self.aprint("Command received: " + command)
 
 
 async def make_async(sync_function):
@@ -46,6 +56,8 @@ async def make_async(sync_function):
 
 if __name__ == "__main__":
     vcs = VCS(System())
-    asyncio.get_event_loop().run_until_complete(vcs.startup())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(vcs.start())
     asyncio.ensure_future(vcs.run())
-    asyncio.get_event_loop().run_forever()
+    loop.run_forever()
+    loop.close()
