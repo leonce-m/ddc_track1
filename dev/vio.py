2d9094373a5aac2222ed8b7f4f24be7ae970d81a
leonce-m <lem2300@thi.de>
2020-11-12 23:35:29 +0100

    Refactor vio with mode enum

--- dev/vio.py
+++ dev/vio.py
@@ -1,22 +1,6 @@
 import logging
-from mavsdk.action import *
 from mavsdk.offboard import *
 from dev import misc
-
-def foo1(verb, nouns):
-    return verb + nouns.join(" ") + ": foo1"
-
-
-def foo2(verb, nouns):
-    return verb + nouns.join(" ") + ": foo2"
-
-
-def foo3(verb, nouns):
-    return verb + nouns.join(" ") + ": foo3"
-
-
-def foo4(verb, nouns):
-    return verb + nouns.join(" ") + ": foo4"
 
 
 class VioComError(Exception):
@@ -24,71 +8,99 @@ class VioComError(Exception):
         self.message = message
 
     def __str__(self):
-        return self.message
+        return f"{type(self).__name__}: {self.message}"
 
-class Grammar(object):
-    VERBS = [
-        ({"climb", "descend", "maintain"}, "ALT", foo1),
-        ({"turn"}, "DIR", foo2),
-        ({"hold"}, "LOC", foo3),
-        ({"direct"}, "LOC", foo4)
-    ]
-    NOUNS = {
-        "ALT": {"FL [0-9]", "[0-9]+ ft"},
-        "DIR": {"heading [0-9]"},
-        "LOC": {"Ingolstadt Main Station", "MIQ", "OTT VOR", "WLD VOR"}
-    }
+class Mode(Enum):
+    ALTITUDE  = 1
+    DIRECTION = 2
+    POSITION  = 3
+    TAKEOFF   = 4
+    LAND      = 6
+    STATUS    = 7
+    SPECIAL   = 8
+
+
+VERBS = {
+    Mode.ALTITUDE:  {"climb", "descend", "maintain"},
+    Mode.DIRECTION: {"turn"},
+    Mode.POSITION:  {"hold, direct"}
+}
+
+NOUNS = {
+    Mode.ALTITUDE:  {"FL [0-9]", "[0-9]+ ft"},
+    Mode.DIRECTION: {"heading [0-9]"},
+    Mode.POSITION:  {"Ingolstadt Main Station", "MIQ", "OTT VOR", "WLD VOR"}
+}
+
+LOCATIONS_NED = {
+    "Ingolstadt Main Station": (0, 0, 2),
+    "MIQ": (1, 1, 2),
+    "OTT VOR": (1, 3, 2),
+    "WLD VOR": (3, 0, 2)
+}
+
+
+def fl_to_m(alt):
+    return alt * 30.48
+
+def ft_to_m(alt):
+    return alt * 0.3048
+
 
 class Parser(object):
 
     def __init__(self, call_sign):
         self.call_sign = call_sign
-        self.verbs = Grammar.VERBS
-        self.nouns = Grammar.NOUNS
+        self.verbs = VERBS
+        self.nouns = NOUNS
         self.phrase = ""
         self.command_list = list()
 
     def find_next_verb(self, token):
         for i, t in enumerate(token):
-            for v_type in self.verbs:
-                for r in v_type[0]:
+            for mode in self.verbs.keys():
+                for r in self.verbs[mode]:
                     if r == t:
-                        return i, t, v_type
-        return None
+                        return i, t, mode.name
+        return 0, 0, 0
 
-    def handle_response(self, phrase, v_type=None):
+    def handle_response(self, phrase, mode=None):
         # TODO: implement proper response decision tree
-        if not v_type:
-            self.phrase += " " + phrase
+        if mode:
+            pass
+        self.phrase += " " + phrase
 
     def handle_response_queue(self):
-        logging.info(f"Response: {self.call_sign},{self.phrase}.")
+        if len(self.phrase) > 0:
+            logging.info(f"Response: {self.phrase.strip().capitalize()}, {self.call_sign}.")
+        else:
+            logging.info(f"Response: {self.call_sign}.")
         self.phrase = ""
 
-    def handle_phrase(self, phrase, v_type):
+    def handle_phrase(self, phrase, mode):
         # TODO: implement proper command selection based on v_type[2]
-        for regex in self.nouns.get(v_type[1], ""):
+        for regex in self.nouns.get(mode, ""):
             pass
         # TODO: regex search for noun params
         # TODO: convert FL and ft to m
-        command = v_type[2]
-        self.command_list.append(command())
+        args = []
+        self.command_list.append((mode, args))
 
     def handle_phrase_queue(self, token):
         if len(token) == 0:
             return
-        i, verb, v_type = self.find_next_verb(token)
-        if not verb:
-            raise VioComError("unable")
+        i, verb1, mode1 = self.find_next_verb(token)
+        if not verb1:
+            raise VioComError(f"Phrase '{' '.join(token)}' does not contain known command")
         token.pop(i)
-        j, temp = self.find_next_verb(token)
-        if not temp:
+        j, verb2, mode2 = self.find_next_verb(token)
+        if not verb2:
             j = len(token)
-        token.insert(i, verb)
+        token.insert(i, verb1)
         phrase = " ".join(token[0:j+1])
         del token[0:j+1]
-        self.handle_response(phrase, v_type)
-        self.handle_phrase(phrase, v_type)
+        self.handle_response(phrase, mode1)
+        self.handle_phrase(phrase, mode1)
         self.handle_phrase_queue(token)
 
     def handle_id(self, token):
@@ -111,7 +123,7 @@ class Parser(object):
                 self.handle_phrase_queue(token)
             except VioComError as e:
                 logging.error(e)
-                self.handle_response(e.message)
+                self.handle_response("Say again")
             finally:
                 self.handle_response_queue()
         return self.command_list
