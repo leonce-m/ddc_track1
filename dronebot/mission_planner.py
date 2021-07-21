ccf6d959d0cbf4144de88c7358b304aac7337194
lem2300 <leonce.mollerus@schanzer-racing.de>
2021-07-21 14:08:48 +0200

    Move Vocabulary class to a separate module

--- dronebot/mission_planner.py
+++ dronebot/mission_planner.py
@@ -1,54 +1,13 @@
 import asyncio
 import logging
 import math
-import re
 import traceback
-from enum import IntEnum
-from pathlib import Path
 
 import utm
-import yaml
 from mavsdk import System, telemetry, action, mission
+from dronebot.vocabulary import Vocabulary
 
-
-class Vocabulary:
-    """
-    Container class for vocabulary loaded from a YAML config file.
-    @DynamicAttrs
-    """
-
-    def __init__(self):
-        with open((Path(__file__).parent / 'vocabulary.yaml').resolve()) as file:
-            vocab = yaml.load(file, Loader=yaml.FullLoader)
-
-        setattr(self, 'MODE', IntEnum('MODE', vocab.get('MODES')))
-        setattr(self, 'VERBS', dict((self.MODE[key], set(val)) for key, val in vocab.get('VERBS').items()))
-        setattr(self, 'NOUNS', dict((self.MODE[key], set(val)) for key, val in vocab.get('NOUNS').items()))
-        setattr(self, 'POSITIONS', dict((key, telemetry.Position(*val)) for key, val in vocab.get('POSITIONS').items()))
-
-    def get_arg(self, pattern, phrase, mode):
-        match = re.search(pattern, phrase)
-        if match:
-            arg = match.group(0)
-            if mode == self.MODE.ALTITUDE:
-                val = match.group('val')
-                unit = match.group('unit')
-                if unit == "FL":
-                    arg = float(val) * 30.48 * 0.01
-                elif unit == "ft":
-                    arg = float(val) * 0.3048 * 0.01
-            if mode == self.MODE.HEADING:
-                val = match.group('val')
-                arg = int(val)
-            if mode == self.MODE.POSITION:
-                arg = self.POSITIONS.get(arg)
-            if mode == self.MODE.LAND:
-                arg = ' '.join([match.group('val'), match.group('unit')])
-                arg = self.POSITIONS.get(arg)
-            return arg
-
-
-class MissionPlanner(Vocabulary):
+class MissionPlanner:
     """
     Provides mavsdk coroutines based on parsed command input.
     """
@@ -57,6 +16,7 @@ class MissionPlanner(Vocabulary):
         super().__init__()
         self.drone = drone
         self.mission_plan = None
+        self.vocab = Vocabulary()
         self.target_alt = 5
 
     @staticmethod
@@ -69,15 +29,15 @@ class MissionPlanner(Vocabulary):
         await asyncio.sleep(0.1)
 
     def fetch_command_coro(self, mode, *args):
-        if mode == self.MODE.ALTITUDE:
+        if mode == self.vocab.MODE.ALTITUDE:
             return self.mission_change_altitude(*args)
-        if mode == self.MODE.POSITION:
+        if mode == self.vocab.MODE.POSITION:
             return self.mission_fly_direct(*args)
-        if mode == self.MODE.HEADING:
+        if mode == self.vocab.MODE.HEADING:
             return self.mission_fly_heading(*args)
-        if mode == self.MODE.TAKEOFF:
+        if mode == self.vocab.MODE.TAKEOFF:
             return self.command_takeoff()
-        if mode == self.MODE.LAND:
+        if mode == self.vocab.MODE.LAND:
             return self.command_landing(*args)
 
     async def get_position(self) -> telemetry.Position:
